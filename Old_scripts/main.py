f5bf13d2a43e7290d5bfa7b8bf11070e023dbb23
Ezenwa Victory <ezenwavictory744@gmail.com>
2024-04-14 21:41:47 +0100

    first commit

---
+++ Old_scripts/main.py
@@ -0,0 +1,13 @@
+items = {
+    1: {
+        "name": "Chair",
+        "price": 17.99
+    },
+    2: {
+        "name": "Table",
+        "price": 180.50
+    }
+}
+print(items.values())
+import secrets
+print(secrets.SystemRandom().getrandbits(128))
