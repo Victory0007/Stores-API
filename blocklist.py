36843bf0bdcc81c8a6aaf03ae94842f3e3f67230
Ezenwa Victory <ezenwavictory744@gmail.com>
2024-04-14 21:46:09 +0100

    second commit

---
+++ blocklist.py
@@ -0,0 +1,3 @@
+#You should use a database to store your blocklist so the blocklist does not get erased everytime you refresh
+# the app. You might want to use redis instead of a database as well
+BLOCKLIST = set()
